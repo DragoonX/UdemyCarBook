4fa40dff4b6fb438f70e7b85a433dbb53ef30509
Anılcan Şaşkın <anilcansaskin@hotmail.com>
2025-01-17 02:48:50 +0300

    Add project files.

---
+++ Core/UdemyCarBook.Domain/Entities/CarDescription.cs
@@ -0,0 +1,10 @@
+﻿namespace UdemyCarBook.Domain.Entities
+{
+    public class CarDescription
+    {
+        public int Id { get; set; }
+        public int CarId { get; set; }
+        public Car Car { get; set; }
+        public string Detail { get; set; }
+    }
+}
---
+++ Core/UdemyCarBook.Domain/Entities/Feature.cs
@@ -0,0 +1,10 @@
+﻿namespace UdemyCarBook.Domain.Entities
+{
+    public class Feature
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public List<CarFeature> CarFeatures { get; set; }
+    }
+}
