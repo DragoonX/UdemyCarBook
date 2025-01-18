6866d073d61411fc03ce25353c46b2db0ad120f4
Anılcan Şaşkın <anilcansaskin@hotmail.com>
2025-01-18 18:40:24 +0300

    created commands and handlers

---
+++ Core/UdemyCarBook.Application/Features/CQRS/Handlers/AboutHandlers/UpdateAboutCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using UdemyCarBook.Application.Features.CQRS.Commands.AboutCommands;
+using UdemyCarBook.Application.Interfaces;
+using UdemyCarBook.Domain.Entities;
+
+namespace UdemyCarBook.Application.Features.CQRS.Handlers.AboutHandlers
+{
+    public class UpdateAboutCommandHandler
+    {
+        private readonly IRepository<About> _repository;
+
+        public UpdateAboutCommandHandler(IRepository<About> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(UpdateAboutCommand command)
+        {
+            var value = await _repository.GetByIdAsync(command.Id);
+            value.Description = command.Description;
+            value.Title = command.Title;
+            value.ImageUrl = command.ImageUrl;
+
+            await _repository.UpdateAsync(value);
+        }
+    }
+}
