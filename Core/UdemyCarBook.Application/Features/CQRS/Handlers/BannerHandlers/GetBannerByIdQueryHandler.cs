1d3038c80ff81a03cd51824e454b6d512bba8fac
Anılcan Şaşkın <anilcansaskin@hotmail.com>
2025-01-18 21:36:07 +0300

    banner rules added

---
+++ Core/UdemyCarBook.Application/Features/CQRS/Handlers/BannerHandlers/GetBannerByIdQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using UdemyCarBook.Application.Features.CQRS.Queries.BannerQueries;
+using UdemyCarBook.Application.Features.CQRS.Results.BannerResults;
+using UdemyCarBook.Application.Interfaces;
+using UdemyCarBook.Domain.Entities;
+
+namespace UdemyCarBook.Application.Features.CQRS.Handlers.BannerHandlers
+{
+    public class GetBannerByIdQueryHandler
+    {
+        private readonly IRepository<Banner> _repository;
+
+        public GetBannerByIdQueryHandler(IRepository<Banner> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<GetBannerByIdQueryResult> Handle(GetBannerByIdQuery query)
+        {
+            var value = await _repository.GetByIdAsync(query.Id);
+
+            return new GetBannerByIdQueryResult
+            {
+                Id = value.Id,
+                Title = value.Title,
+                Description = value.Description,
+                VideoDescription = value.VideoDescription,
+                VideoUrl = value.VideoUrl
+            };
+        }
+    }
+}
