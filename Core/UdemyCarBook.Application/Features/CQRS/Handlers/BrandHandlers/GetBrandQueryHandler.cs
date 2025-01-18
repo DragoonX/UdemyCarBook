2f388d7a1ce71424355b6a21bf0266e6519dd55a
Anılcan Şaşkın <anilcansaskin@hotmail.com>
2025-01-18 23:42:28 +0300

    brand cqrs added

---
+++ Core/UdemyCarBook.Application/Features/CQRS/Handlers/BrandHandlers/GetBrandQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using UdemyCarBook.Application.Features.CQRS.Results.BrandResults;
+using UdemyCarBook.Application.Interfaces;
+using UdemyCarBook.Domain.Entities;
+
+namespace UdemyCarBook.Application.Features.CQRS.Handlers.BrandHandlers
+{
+    public class GetBrandQueryHandler
+    {
+        private readonly IRepository<Brand> _repository;
+
+        public GetBrandQueryHandler(IRepository<Brand> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<List<GetBrandQueryResult>> Handle()
+        {
+            var values = await _repository.GetAllAsync();
+            return values.ConvertAll(x => new GetBrandQueryResult
+            {
+                Id = x.Id,
+                Name = x.Name
+            });
+        }
+    }
+}
