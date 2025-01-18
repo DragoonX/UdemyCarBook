f8517700297a90d2c47aacb87c4bae6e9f42a0fe
Anılcan Şaşkın <anilcansaskin@hotmail.com>
2025-01-18 18:22:29 +0300

    created handlers and results

---
+++ Core/UdemyCarBook.Application/Features/CQRS/Handlers/AboutHandlers/GetAboutByIdQueryHandler.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyCarBook.Application.Features.CQRS.Queries.AboutQueries;
+using UdemyCarBook.Application.Features.CQRS.Results.AboutResults;
+using UdemyCarBook.Application.Interfaces;
+using UdemyCarBook.Domain.Entities;
+
+namespace UdemyCarBook.Application.Features.CQRS.Handlers.AboutHandlers
+{
+    public class GetAboutByIdQueryHandler
+    {
+        private readonly IRepository<About> _repository;
+
+        public GetAboutByIdQueryHandler(IRepository<About> repository)
+        {
+            _repository = repository;
+        }
+
+        public GetAboutByIdQueryResult Handle(GetAboutByIdQuery query)
+        {
+            var value = _repository.GetByIdAsync(query.Id).Result;
+            return new GetAboutByIdQueryResult
+            {
+                Id = value.Id,
+                Title = value.Title,
+                Description = value.Description,
+                ImageUrl = value.ImageUrl,
+            };
+        }
+    }
+}
---
+++ Core/UdemyCarBook.Application/Features/CQRS/Handlers/AboutHandlers/GetAboutQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using UdemyCarBook.Application.Features.CQRS.Results.AboutResults;
+using UdemyCarBook.Application.Interfaces;
+using UdemyCarBook.Domain.Entities;
+
+namespace UdemyCarBook.Application.Features.CQRS.Handlers.AboutHandlers
+{
+    public class GetAboutQueryHandler
+    {
+        private readonly IRepository<About> _repository;
+
+        public GetAboutQueryHandler(IRepository<About> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<List<GetAboutQueryResult>> Handle()
+        {
+            var values = await _repository.GetAllAsync();
+            return values.ConvertAll(x => new GetAboutQueryResult
+            {
+                Id = x.Id,
+                Title = x.Title,
+                Description = x.Description,
+                ImageUrl = x.ImageUrl,
+            });
+        }
+    }
+}
