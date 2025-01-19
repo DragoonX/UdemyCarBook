a05d7a0babe1a67a3a9a7d178db7764c6ca67b66
Anılcan Şaşkın <anilcansaskin@hotmail.com>
2025-01-19 22:19:55 +0300

    category cqrs

---
+++ Core/UdemyCarBook.Application/Features/CQRS/Handlers/CategoryHandlers/GetCategoryQueryHandler.cs
@@ -0,0 +1,20 @@
+﻿using UdemyCarBook.Application.Interfaces;
+using UdemyCarBook.Domain.Entities;
+
+namespace UdemyCarBook.Application.Features.CQRS.Handlers.CategoryHandlers
+{
+    public class GetCategoryQueryHandler
+    {
+        private readonly IRepository<Category> _repository;
+
+        public GetCategoryQueryHandler(IRepository<Category> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<List<Category>> Handle()
+        {
+            return await _repository.GetAllAsync();
+        }
+    }
+}
