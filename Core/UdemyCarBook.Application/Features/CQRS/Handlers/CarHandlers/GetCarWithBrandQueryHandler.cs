3528bc605aa74c0320bc5ecdd45d71315149f20e
Anılcan Şaşkın <anilcansaskin@hotmail.com>
2025-01-19 21:51:09 +0300

    getcarwithbrands include and repositories

---
+++ Core/UdemyCarBook.Application/Features/CQRS/Handlers/CarHandlers/GetCarWithBrandQueryHandler.cs
@@ -0,0 +1,34 @@
+﻿using UdemyCarBook.Application.Features.CQRS.Results.CarResults;
+using UdemyCarBook.Application.Interfaces.CarInterfaces;
+
+namespace UdemyCarBook.Application.Features.CQRS.Handlers.CarHandlers
+{
+    public class GetCarWithBrandQueryHandler
+    {
+        private readonly ICarRepository _carRepository;
+
+        public GetCarWithBrandQueryHandler(ICarRepository carRepository)
+        {
+            _carRepository = carRepository;
+        }
+
+        public List<GetCarWithBrandQueryResult> Handle()
+        {
+            var value = _carRepository.GetCarsListWithBrands();
+            return value
+                .ConvertAll(x => new GetCarWithBrandQueryResult
+                {
+                    Id = x.Id,
+                    BigImageUrl = x.BigImageUrl,
+                    BrandId = x.BrandId,
+                    CoverImageUrl = x.CoverImageUrl,
+                    Fuel = x.Fuel,
+                    Km = x.Km,
+                    Luggage = x.Luggage,
+                    Model = x.Model,
+                    Seat = x.Seat,
+                    BrandName = x.Brand.Name
+                });
+        }
+    }
+}
---
+++ Infrastructure/UdemyCarBook.Persistence/Repositories/CarRepositories/CarRepository.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UdemyCarBook.Application.Interfaces.CarInterfaces;
+using UdemyCarBook.Domain.Entities;
+using UdemyCarBook.Persistence.Context;
+
+namespace UdemyCarBook.Persistence.Repositories.CarRepositories
+{
+    public class CarRepository : ICarRepository
+    {
+        private readonly CarBookContext _context;
+
+        public CarRepository(CarBookContext context)
+        {
+            _context = context;
+        }
+
+        public List<Car> GetCarsListWithBrands()
+        {
+            return _context.Cars.Include(x => x.Brand).ToList();
+        }
+    }
+}
