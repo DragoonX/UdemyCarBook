980da6bd4afc126f99122585b89fccce34338617
Anılcan Şaşkın <anilcansaskin@hotmail.com>
2025-01-25 00:28:46 +0300

    footer address mediator cqrs

---
+++ Core/UdemyCarBook.Application/Features/Mediator/Handlers/FooterAddressHandlers/UpdateFooterAddressCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using UdemyCarBook.Application.Features.Mediator.Commands.FooterAddressCommands;
+using UdemyCarBook.Application.Interfaces;
+using UdemyCarBook.Domain.Entities;
+
+namespace UdemyCarBook.Application.Features.Mediator.Handlers.FooterAddressHandlers
+{
+    public class UpdateFooterAddressCommandHandler : IRequestHandler<UpdateFooterAddressCommand>
+    {
+        private readonly IRepository<FooterAddress> _repository;
+
+        public UpdateFooterAddressCommandHandler(IRepository<FooterAddress> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(UpdateFooterAddressCommand request, CancellationToken cancellationToken)
+        {
+            var value = await _repository.GetByIdAsync(request.Id);
+
+            value.Description = request.Description;
+            value.Address = request.Address;
+            value.Phone = request.Phone;
+            value.Email = request.Email;
+            await _repository.UpdateAsync(value);
+        }
+    }
+}
