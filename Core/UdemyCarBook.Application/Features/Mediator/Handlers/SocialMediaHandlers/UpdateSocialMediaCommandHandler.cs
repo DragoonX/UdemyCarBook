df38f98dac52d602a12a0d1dae9cbbfda8af5ca8
Anılcan Şaşkın <anilcansaskin@hotmail.com>
2025-01-25 03:02:19 +0300

    social media mediator cqrs

---
+++ Core/UdemyCarBook.Application/Features/Mediator/Handlers/SocialMediaHandlers/UpdateSocialMediaCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using UdemyCarBook.Application.Features.Mediator.Commands.SocialMediaCommands;
+using UdemyCarBook.Application.Interfaces;
+using UdemyCarBook.Domain.Entities;
+
+namespace UdemyCarBook.Application.Features.Mediator.Handlers.SocialMediaHandlers
+{
+    public class UpdateSocialMediaCommandHandler : IRequestHandler<UpdateSocialMediaCommand>
+    {
+        private readonly IRepository<SocialMedia> _repository;
+
+        public UpdateSocialMediaCommandHandler(IRepository<SocialMedia> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(UpdateSocialMediaCommand request, CancellationToken cancellationToken)
+        {
+            var value = await _repository.GetByIdAsync(request.Id);
+            value.Url = request.Url;
+            value.Name = request.Name;
+            value.Icon = request.Icon;
+            await _repository.UpdateAsync(value);
+        }
+    }
+}
